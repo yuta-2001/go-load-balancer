42a279796d15dbce2c818a0e8ff4c7e01093bf71
yuta-2001 <yu0210ta.sug@gmail.com>
2024-10-27 00:26:32 +0900

    serve関数

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/yuta-2001/go-load-balancer
+
+go 1.20
--- main.go
+++ main.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"log"
+	"net/http"
+	"net/http/httputil"
+)
+
+func Serve() {
+	director := func(req *http.Request) {
+		req.URL.Scheme = "http"
+		req.URL.Host = ":8081"
+	}
+
+	rq := &httputil.ReverseProxy{
+		Director: director,
+	}
+
+	s := http.Server{
+		Addr:    ":8080",
+		Handler: rq,
+	}
+
+	if err := s.ListenAndServe(); err != nil {
+		log.Fatal(err.Error())
+	}
+}
