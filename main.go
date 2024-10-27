b8a46ebba81e70f71b8f4f3ea0eeb616ab08414b
yuta-2001 <yu0210ta.sug@gmail.com>
2024-10-27 11:58:32 +0900

    load confg and lb

--- main.go
+++ main.go
@@ -4,24 +4,61 @@ import (
 	"log"
 	"net/http"
 	"net/http/httputil"
+
+	"sync"	
+	"io/ioutil"
+	"encoding/json"
+	"net/url"
 )
 
-func Serve() {
-	director := func(req *http.Request) {
-		req.URL.Scheme = "http"
-		req.URL.Host = ":8081"
-	}
+type Config struct {
+	Proxy Proxy `json:"proxy"`
+	Backends []Backend `json:"backends"`
+}
 
-	rq := &httputil.ReverseProxy{
-		Director: director,
+type Proxy struct {
+	Port string `json:"port"`
+}
+
+type Backend struct {
+	URL string `json:"url"`
+	IsDead bool
+	mu sync.RWMutex
+}
+
+var mu sync.Mutex
+var idx int = 0
+
+func IbHandler(w http.ResponseWriter, r *http.Request) {
+	maxLen := len(cfg.Backends)
+
+	mu.Lock()
+	// currentBackend := cfg.Backends[idx%maxLen]
+	targetUrl, err := url.Parse(cfg.Backends[idx%maxLen].URL)
+	if err != nil {
+		log.Fatal(err.Error())
 	}
+	idx++
+	mu.Unlock()
+	reverseProxy := httputil.NewSingleHostReverseProxy(targetUrl)
+	reverseProxy.ServeHTTP(w, r)
+}
+
+var cfg Config
+
+func Serve() {
+	data, err := ioutil.ReadFile("./config.json")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	json.Unmarshal(data, &cfg)
 
 	s := http.Server{
-		Addr:    ":8080",
-		Handler: rq,
+		Addr: ":" + cfg.Proxy.Port,
+		Handler: http.HandlerFunc(IbHandler),
 	}
 
-	if err := s.ListenAndServe(); err != nil {
+	if err = s.ListenAndServe(); err != nil {
 		log.Fatal(err.Error())
 	}
 }
